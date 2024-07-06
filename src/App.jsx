e7c110d8bc733f119bdc1486171efb91be0e4976
Abdelrahman Elkhateeb <109158103+abdelrahman-elkhateeb@users.noreply.github.com>
2024-07-06 15:10:45 +0300

    upload app

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./features/ui/Home";
+import Menu from "./features/menu/Menu";
+import CreateOrder from "./features/order/CreateOrder";
+import Order from "./features/order/Order";
+import Cart from "./features/cart/Cart";
+
+const router = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "/menu", element: <Menu /> },
+  { path: "./cart", element: <Cart /> },
+  { path: "./order/new", element: <CreateOrder /> },
+  { path: "./order/:orderId", element: <Order /> },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
