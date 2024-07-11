0ccb5fd8a04e9bafa06d833e0a0fcf11673990f4
Abdelrahman Elkhateeb <109158103+abdelrahman-elkhateeb@users.noreply.github.com>
2024-07-12 01:22:43 +0300

    adding styling

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,4 +1,5 @@
 import { formatCurrency } from "../../utilities/helpers";
+import Button from "../ui/Button";
 
 function MenuItem({ pizza }) {
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
@@ -10,18 +11,21 @@ function MenuItem({ pizza }) {
         alt={name}
         className={`h-24 ${soldOut ? "opacity-70 grayscale" : ""}`}
       />
-
-      <div className="flex flex-col">
-        <p>{name}</p>
-        <p>{ingredients.join(", ")}</p>
-        <div className="mt-auto">
+      <div className="flex grow flex-col pt-0.5">
+        <p className="font-medium">{name}</p>
+        <p className="text-sm capitalize italic text-stone-500">
+          {ingredients.join(", ")}
+        </p>
+        <div className="mt-auto flex items-center justify-between">
           {!soldOut ? (
-            <p className="text-sm font-medium uppercase text-stone-500">
-              {formatCurrency(unitPrice)}
-            </p>
+            <p className="text-sm">{formatCurrency(unitPrice)}</p>
           ) : (
-            <p>Sold out</p>
+            <p className="text-sm font-medium uppercase text-stone-500">
+              Sold out
+            </p>
           )}
+
+          <Button type="small">Add to cart</Button>
         </div>
       </div>
     </li>
