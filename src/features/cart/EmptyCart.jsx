08ce62548c9378e8088cb68533866e22c7e26fdc
abdelrahman elkhateeb <109158103+abdelrahman-elkhateeb@users.noreply.github.com>
2024-08-06 18:56:12 +0300

    handling adding items to the cart and cart view

--- src/features/cart/EmptyCart.jsx
+++ src/features/cart/EmptyCart.jsx
@@ -1,11 +1,14 @@
-import { Link } from 'react-router-dom';
-
+import LinkButton from "../ui/LinkButton";
+import sad from "../../../public/sad.svg";
 function EmptyCart() {
   return (
-    <div>
-      <Link to="/menu">&larr; Back to menu</Link>
+    <div className="px-4 py-3">
+      <LinkButton to="/menu">&larr; Back to menu</LinkButton>
 
-      <p>Your cart is still empty. Start adding some pizzas :)</p>
+      <img src={sad} alt="sad" />
+      <p className="text-center text-xl font-semibold capitalize text-yellow-500">
+        Your cart is still empty...
+      </p>
     </div>
   );
 }
